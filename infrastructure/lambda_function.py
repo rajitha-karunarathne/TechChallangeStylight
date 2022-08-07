af30c4a4ab85d9d095987e142d012d38900fce97
rajitha-karunarathne <root@342-MC-G6VTKPJ6W7.local>
2022-08-08 00:38:01 +0530

    Added the Dynamo DB Infra Changes to the Stylight Application

--- infrastructure/lambda_function.py
+++ infrastructure/lambda_function.py
@@ -25,8 +25,13 @@ if os.environ.get("SNS_TOPIC_ARN") is not None:
 else:
     raise Exception("SNS_TOPIC_ARN parameter is required")
 
+if os.environ.get("DB_TABLE") is not None:
+    db_table = os.environ["DB_TABLE"]
+else:
+    raise Exception("DB_TABLE parameter is required")
+
 #Calculate the Cost Usage and Output
-def usage_calculation(service,billing_client,your_estimation):
+def usage_calculation(service,billing_client,your_estimation,db_client,db_name):
     
     # getting dates (yyyy-MM-dd) and converting to string
     today = datetime.date.today()
@@ -72,7 +77,8 @@ def usage_calculation(service,billing_client,your_estimation):
             cost_usage.append(line)
         
     return cost_usage
-    
+
+#Send SNS Email   
 def send_sns_email(sns_messege,sns,sns_topic_arn):
     response = sns.publish(
     TopicArn=sns_topic_arn,
@@ -81,6 +87,13 @@ def send_sns_email(sns_messege,sns,sns_topic_arn):
     
     print(response)
 
+#Update the DB
+def update_db_table(db_client,db_name,name,amount,str_today,str_weekback):
+    table = db_client.Table(db_name)
+    datetimestamp = str(datetime.datetime.now())
+    amount = str("%.2f" % amount)
+    table.put_item(Item= {'EntryDateTime': datetimestamp,'ResourceName': name,'Amount': amount,'FromDate': str_weekback,'ToDate': str_today })
+
 def lambda_handler(event, context):
     ########### Getting the Amount spent in AWS ########### 
     # opening connection with cost explorer 
@@ -88,12 +101,15 @@ def lambda_handler(event, context):
     
     # Create an SNS client
     sns = boto3.client('sns')
+
+    # Create an DynamoDB client
+    db_client = boto3.resource('dynamodb')
     
     # Get EC2 Expensive
-    ec2_usage_msg = usage_calculation('Amazon Elastic Compute Cloud - Compute',billing_client,ec2_estimation)
+    ec2_usage_msg = usage_calculation('Amazon Elastic Compute Cloud - Compute',billing_client,ec2_estimation,db_client,db_table)
     
     # Get RDS Expensive
-    rds_usage_msg = usage_calculation('Relational Database Service (RDS)',billing_client,rds_estimation)
+    rds_usage_msg = usage_calculation('Relational Database Service (RDS)',billing_client,rds_estimation,db_client,db_table)
 
     # Generate the Warning Messege
     sns_messege = f"Following EC2s have exceeded the Cost threshold, {ec2_usage_msg}\n\n Following RDSs have exceeded the Cost threshold, {rds_usage_msg}"
